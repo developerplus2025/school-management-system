7b09bc4b735f3af524885c690f0634a1edf28e1d
Phạm Quang Trường An <139692078+developerplus2025@users.noreply.github.com>
2025-11-02 10:22:08 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="${geistSans.variable} ${geistMono.variable}" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} h-screen flex flex-col gap-8 items-center  antialiased`}
       >
